3f773e56a7fba40632712e541cc3d7c584d33a37
TheSned <dsnedecor@gmail.com>
2013-05-03 11:09:35 -0500

    Unit tests for https://github.com/Grandpappy/eXeMeL/issues/6.

--- src/eXeMeL/eXeMeL.Tests/VisualStudioVBScriptCleanerTests.cs
+++ src/eXeMeL/eXeMeL.Tests/VisualStudioVBScriptCleanerTests.cs
@@ -19,9 +19,33 @@ namespace eXeMeL.Tests
     }
 
     [TestMethod]
+    public void CleanXml_NonEscapedEmptyXmlAttributeWithSeparateClosingTag_ShouldNotReplaceDoubleQuotes()
+    {
+      const string sample = "<xml attr=\"\" ></xml>";
+      var context = new XmlCleanerContext() { XmlToClean = sample };
+
+      var sut = new VisualStudioVBScriptCleaner();
+      sut.CleanXml(context);
+
+      Assert.AreEqual(sample, context.XmlToClean);
+    }
+
+    [TestMethod]
     public void CleanXml_NonEscapedEmptyXmlAttributeWithNoWhitespaceAfter_ShouldNotReplaceDoubleQuotes()
     {
       const string sample = "<xml attr=\"\"/>";
+      var context = new XmlCleanerContext() { XmlToClean = sample };
+
+      var sut = new VisualStudioVBScriptCleaner();
+      sut.CleanXml(context);
+
+      Assert.AreEqual(sample, context.XmlToClean);
+    }
+
+    [TestMethod]
+    public void CleanXml_NonEscapedEmptyXmlAttributeWithNoWhitespaceAfterWithSeparateClosingTag_ShouldNotReplaceDoubleQuotes()
+    {
+      const string sample = "<xml attr=\"\"></xml>";
       var context = new XmlCleanerContext() { XmlToClean = sample };
 
       var sut = new VisualStudioVBScriptCleaner();
@@ -44,6 +68,19 @@ namespace eXeMeL.Tests
     }
 
     [TestMethod]
+    public void CleanXml_EscapedEmptyXmlAttributeWithSeparateClosingTag_ShouldReplaceDoubleQuotes()
+    {
+      const string sample = "<xml attr=\"\"\"\" ></xml>";
+      const string expected = "<xml attr=\"\" ></xml>";
+      var context = new XmlCleanerContext() { XmlToClean = sample };
+
+      var sut = new VisualStudioVBScriptCleaner();
+      sut.CleanXml(context);
+
+      Assert.AreEqual(expected, context.XmlToClean);
+    }
+
+    [TestMethod]
     public void CleanXml_EscapedNonEmptyXmlAttribute_ShouldReplaceDoubleQuotes()
     {
       const string sample = "<xml attr=\"\"Test\"\" />";
@@ -57,10 +94,36 @@ namespace eXeMeL.Tests
     }
 
     [TestMethod]
+    public void CleanXml_EscapedNonEmptyXmlAttributeWithSeparateClosingTag_ShouldReplaceDoubleQuotes()
+    {
+      const string sample = "<xml attr=\"\"Test\"\" ></xml>";
+      const string expected = "<xml attr=\"Test\" ></xml>";
+      var context = new XmlCleanerContext() { XmlToClean = sample };
+
+      var sut = new VisualStudioVBScriptCleaner();
+      sut.CleanXml(context);
+
+      Assert.AreEqual(expected, context.XmlToClean);
+    }
+
+    [TestMethod]
     public void CleanXml_EscapedNonEmptyXmlAttributeWithNoWhitespaceAfter_ShouldReplaceDoubleQuotes()
     {
       const string sample = "<xml attr=\"\"Test\"\"/>";
       const string expected = "<xml attr=\"Test\"/>";
+      var context = new XmlCleanerContext() { XmlToClean = sample };
+
+      var sut = new VisualStudioVBScriptCleaner();
+      sut.CleanXml(context);
+
+      Assert.AreEqual(expected, context.XmlToClean);
+    }
+
+    [TestMethod]
+    public void CleanXml_EscapedNonEmptyXmlAttributeWithNoWhitespaceAfterWithSeparateClosingTag_ShouldReplaceDoubleQuotes()
+    {
+      const string sample = "<xml attr=\"\"Test\"\"></xml>";
+      const string expected = "<xml attr=\"Test\"></xml>";
       var context = new XmlCleanerContext() { XmlToClean = sample };
 
       var sut = new VisualStudioVBScriptCleaner();
